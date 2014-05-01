93d805100e883963063e5950e48662b4eb862340
James Woglom <j@wogloms.net>
2014-04-30 21:39:39 -0400

    mailgun

--- firebaseaction.php
+++ firebaseaction.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'private.inc.php';
 if(isset($_GET['debug'])) {
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
@@ -29,26 +30,45 @@ $emailTemplate = <<<EOF
     </div>
 </div>
 EOF;
+function sendmail($to, $title, $msg, $headers) {
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+curl_setopt($ch, CURLOPT_USERPWD, 'api:'.MAILGUN_SECRET_KEY);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_URL, 'https://api.mailgun.net/v2/sandboxbe99f4f2f16142c18000a86a62a6f14b.mailgun.org/messages');
+curl_setopt($ch, CURLOPT_POSTFIELDS, array('from' => 'postmaster@sandboxbe99f4f2f16142c18000a86a62a6f14b.mailgun.org',
+'to' => $to,
+'subject' => $title,
+'text' => $msg));
+curl_setopt($ch, CURLOPT_HTTPHEADER, explode("\r\n",$headers));
+$result = curl_exec($ch);
+curl_close($ch); 
+}
 function sendEmail($email, $title, $content, $messageattrs) {
     global $emailTemplate;
     $message = str_replace("{{content}}", $content, $emailTemplate);
     $message = str_replace(array_keys($messageattrs), array_values($messageattrs), $message);
     echo "Sending: $message";
-    mail($email, $title, $message, "From: noreply@eventifi.co");
+    $headers  = 'MIME-Version: 1.0' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    sendmail($email, $title, $message, $headers."From: noreply@eventifi.co");
 }
 function sendTempEmail($data) {
     $req = file_get_contents("http://eventifiapp.comeze.com/eventifi_email_send.php?".http_build_query($data));
 }
 function sendEmailConfirm($user) {
-    sendTempEmail(array(
-        "email"=>$user['email'], 
-        "title"=>"Eventifi Email Confirmation", 
-        "content"=>"<p>Hello there, someone by the name of {{name}} created an account on Eventifi with this email address. To confirm this registration, click on the following link:<br/><center><button onclick=\"location.href='{{url}}'\" style='font-size:32px'>Confirm</button></center>",
-        
-            "name"=>$user['name'],
-            "tok"=>$user['confirmToken']
-        
-    ));
+    sendEmail(
+        $user['email'], 
+        "Eventifi Email Confirmation", 
+        "<p>Hello there, someone by the name of {{name}} created an account on Eventifi with this email address. To confirm this registration, click on the following link:<br/><center><button onclick=\"location.href='{{url}}'\" style='font-size:32px'>Confirm</button></center>",
+        array(
+            "{{name}}"=>$user['name'],
+            "{{url}}"=>"http://www.eventifi.co/emailConfirm.php?tok=".$user['confirmToken']
+        )
+    );
 }
 
 $act = (isset($_POST['act']) ? $_POST['act'] : $_GET['act']);
